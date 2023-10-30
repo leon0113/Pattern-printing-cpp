7f425f17415416fad0183aa2bf7cae0461b37b8f
Leon <tahjib.leon399@gmail.com>
2023-10-30 10:16:17 +0600

    first

--- first.cpp
+++ first.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
+int main(){
     for(int row = 1; row <= 5; row++){
         for(int col = 1; col <= 5; col++){
             cout<<"@"<<" ";
