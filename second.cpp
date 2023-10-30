aed9d6ac00149c8e96e4e5bdc8c1d1d8cc90b34b
Leon <tahjib.leon399@gmail.com>
2023-10-30 10:28:36 +0600

    Print pattern number

---
+++ second.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int row = 1; row <= 3; row++){
+        for(int col = 1; col <= 5; col++){
+            cout<<"@"<<"     ";
+        }
+        cout<<endl;
+    }
+}
