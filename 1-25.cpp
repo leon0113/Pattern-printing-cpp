29415353c09f29771f223f32556616542bf54a7a
Leon <tahjib.leon399@gmail.com>
2023-10-31 12:50:21 +0600

    num 1-25

--- 1-25.cpp
+++ 1-25.cpp
@@ -1,3 +1,17 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int count = 1;
+//       for(int row = 1; row <= 5; row++){
+//         for(int col = 1; col <= 5; col++){
+//             cout<<count<<" ";
+//             count++;
+//         }
+//         cout<<endl;
+//     }
+// }
+
 #include<iostream>
 using namespace std;
 
@@ -5,8 +19,7 @@ int main(){
     int count = 1;
       for(int row = 1; row <= 5; row++){
         for(int col = 1; col <= 5; col++){
-            cout<<count<<" ";
-            count++;
+            cout<<(row - 1)*5 + col<<" ";
         }
         cout<<endl;
     }
