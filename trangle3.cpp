cc114bf2e46332d043bd34265c6a283384216bea
Leon <tahjib.leon399@gmail.com>
2023-11-04 06:20:39 +0600

    trangle 2

--- trangle3.cpp
+++ trangle3.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     for(int row = 1; row <= 5; row++){
-        for(int col = 1; col<=row; col++){
+        for(int col = row; col>=1; col--){
             cout <<col<<" ";
         }
         cout<<endl;
