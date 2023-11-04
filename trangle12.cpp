8d0c5b40e2ab03c7dce7d0f2ea44e17f9c661dae
Leon <tahjib.leon399@gmail.com>
2023-11-05 00:35:15 +0600

    trangle 12

--- trangle12.cpp
+++ trangle12.cpp
@@ -8,10 +8,13 @@ int main() {
 
     for(int row = 1; row <= n; row++){
         for(int col = 1; col <= (n-row); col++){
-            cout<<" ";
+            cout<<"  ";
         }
-        for(int col = 1; col <= (2*row-1); col++){
-            cout<<"*";
+        for(int col = 1; col <= row; col++){
+           cout<<col<<" ";
+        }
+        for(int col = row - 1; col >= 1; col--){
+           cout<<col<<" ";
         }
         cout<<endl;
     }
