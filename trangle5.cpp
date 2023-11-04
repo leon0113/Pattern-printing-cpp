7ed6e3ae4c7368ba431c2a8653a3c619f2800a65
Leon <tahjib.leon399@gmail.com>
2023-11-04 06:40:00 +0600

    trangle 5

--- trangle5.cpp
+++ trangle5.cpp
@@ -9,3 +9,15 @@ int main(){
         cout<<endl;
     }
 }
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     for(int row = 1; row <= 5; row++){
+//         for(int col = 1; col<= 5-(row-1); col++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+// }
