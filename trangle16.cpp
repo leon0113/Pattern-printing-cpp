ddf6a4d170e223f8de03cd4f05ea882755e8a8d0
Leon <tahjib.leon399@gmail.com>
2023-11-05 17:57:32 +0600

    trangle 16

---
+++ trangle16.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+     
+      for(int row = 1; row <= n; row++){
+
+        for(int col = 1; col <=(n-row); col++){
+           cout<<" ";
+        }
+        for(int col = 1; col <= row; col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+     
+      for(int row = n; row >= 1; row--){
+
+        for(int col = 1; col <=(n-row); col++){
+           cout<<" ";
+        }
+        for(int col = 1; col <= row; col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+   
+}
